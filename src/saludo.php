604e01c52da9727b8ea39a816738186a87f0ca5c
Serhii de la escuela GRI <hztpaserg@gmail.com>
2024-10-18 15:38:57 +0200

    El Commit para sakudo.php -> ->msg

--- src/saludo.php
+++ src/saludo.php
@@ -3,14 +3,22 @@
 
     Class Saludo {
         private $msg;
+        private $usuario;
 
-        public function __construct($msg) {
-            $this->msg = $msg;
+        public function __construct($usuario) {
+            $this->usuario = $usuario;
             $this->setSaludo();
         }
 
         private function setSaludo() {
-            $this->msg = 'Hola Mundo!';
+            $this->msg = '
+                <h2 class="card-title text-primary">Usuario:</h2>
+                <p class="card-text">
+                    <span class="fw-bold text-info">
+                        ' . $this->usuario . ' 
+                    </span>
+                </p>
+            ';
         }
 
         public function getSaludo() {
